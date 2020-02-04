fb6c21250f05bbacf1ebeecb9b545f80d2457799
mohamadreza jafarzade <mrezajafarzade98@gmail.com>
2020-02-04 18:20:23 +0330

    fix bugs

--- app/src/main/java/com/mhmdreza/azmoonyar/views/InfoFragment.kt
+++ app/src/main/java/com/mhmdreza/azmoonyar/views/InfoFragment.kt
@@ -1,17 +1,20 @@
 package com.mhmdreza.azmoonyar.views
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
 import androidx.navigation.Navigation.findNavController
-import com.mhmdreza.azmoonyar.R
 import kotlinx.android.synthetic.main.fragment_info.*
+import android.view.animation.Animation
+import android.view.animation.Transformation
+import android.widget.LinearLayout
+import com.mhmdreza.azmoonyar.R
 
-class InfoFragment : Fragment() {
+
+open class InfoFragment : Fragment() {
 
     private val navController by lazy { findNavController(view!!) }
 
@@ -27,6 +30,39 @@ class InfoFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         backIcon.setOnClickListener{  navController.popBackStack(R.id.mainFragment, false)
         }
+//        val layoutParams = sponsorImageView.layoutParams as LinearLayout.LayoutParams
+//        val topMargin = layoutParams.topMargin
+//        val bottomMargin = layoutParams.bottomMargin
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            scrollView.setOnScrollChangeListener { _, _, scrollY, _, oldScrollY ->
+//                val a = object : Animation() {
+//                     override fun applyTransformation(interpolatedTime: Float, t: Transformation) {
+//                         (sponsorImageView.layoutParams as LinearLayout.LayoutParams).apply {
+//                             setMargins(leftMargin, (topMargin / 2 * interpolatedTime).toInt(),
+//                                 rightMargin, (bottomMargin / 2 * interpolatedTime).toInt())
+//                         }
+//                    }
+//                    override fun getScaleFactor() = 0.5f
+//                    override fun getDuration() = 500L
+//                }
+//                val b = object : Animation() {
+//                     override fun applyTransformation(interpolatedTime: Float, t: Transformation) {
+//                         (sponsorImageView.layoutParams as LinearLayout.LayoutParams).apply {
+//                             setMargins(leftMargin, (topMargin * interpolatedTime).toInt(),
+//                                 rightMargin, (bottomMargin * interpolatedTime).toInt())
+//                         }
+//                    }
+//                    override fun getScaleFactor() = 2f
+//                    override fun getDuration() = 500L
+//                }
+//                if (scrollY in 0..100 && oldScrollY > 100){
+//                    sponsorImageView.startAnimation(b)
+//                } else if (scrollY in 0..100 && oldScrollY > 100) {
+//                    sponsorImageView.startAnimation(a)
+//
+//                }
+//            }
+//        }
     }
 
 
