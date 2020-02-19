b18208c5d440e799ed18ef191fb195af7d1d4108
mohamadreza jafarzade <mrezajafarzade98@gmail.com>
2020-02-19 17:14:07 +0330

    fix keyboard bug

--- app/src/main/java/com/mhmdreza/azmoonyar/views/NameFragment.kt
+++ app/src/main/java/com/mhmdreza/azmoonyar/views/NameFragment.kt
@@ -1,19 +1,27 @@
 package com.mhmdreza.azmoonyar.views
 
 
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.ResultReceiver
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import android.view.inputmethod.InputMethodManager
+import android.widget.RelativeLayout
 import android.widget.Toast
-import androidx.navigation.NavOptions
+import androidx.core.view.updateMargins
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
-
 import com.mhmdreza.azmoonyar.R
 import com.mhmdreza.azmoonyar.data.SharedPref
-import ir.pec.mpl.pecpayment.view.PaymentInitiator
 import kotlinx.android.synthetic.main.fragment_name.*
+import kotlinx.android.synthetic.main.fragment_name.view.*
+
 
 /**
  * A simple [Fragment] subclass.
@@ -30,10 +38,32 @@ class NameFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_name, container, false)
     }
 
+    private fun showSoftKeyboard(view: View) {
+        if (view.requestFocus()) {
+            val imm =
+                activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(
+                view,
+                InputMethodManager.SHOW_FORCED,
+                object : ResultReceiver(Handler()) {
+                    override fun onReceiveResult(resultCode: Int, resultData: Bundle?) {
+                        rootLayout.viewTreeObserver
+                            .addOnGlobalLayoutListener {
+                                val r = Rect()
+                                rootLayout.getWindowVisibleDisplayFrame(r)
+                                rootLayout.layoutParams.height = r.bottom - r.top
+                                rootLayout.invalidate()
+                                rootLayout.requestLayout()
+                            }
+                    }
+                })
+
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        nameEditText.requestFocus()
-
+        showSoftKeyboard(nameEditText)
         submit.setOnClickListener {
             val text = nameEditText.text.toString()
             if (text.isNotEmpty()) {
